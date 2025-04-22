658685414848286f83ed4a23e91c320ac5e913d6
Jim Fowler <fowler@math.osu.edu>
2025-04-22 14:26:53 -0400

    chore: Update Nix shell configuration for Hakyll project dependencies

--- shell.nix
+++ shell.nix
@@ -3,9 +3,11 @@
 pkgs.haskellPackages.shellFor {
   # Ensure the project, including Hakyll, is available in your Nix environment
   packages = p: [ p.hakyll ];
-  tools = [ pkgs.stack ];
-  buildInputs = [ pkgs.zlib pkgs.gcc ];
+  tools = [ pkgs.stack pkgs.unzip ];
+  buildInputs = [ pkgs.zlib  ];
   shellHook = ''
-    export LIBRARY_PATH=${pkgs.zlib}/lib:$LIBRARY_PATH
+    export PKG_CONFIG_PATH="${pkgs.zlib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+    export C_INCLUDE_PATH="${pkgs.zlib.dev}/include:$C_INCLUDE_PATH"
+    export LIBRARY_PATH="${pkgs.zlib.dev}/lib:$LIBRARY_PATH"
   '';
 }
