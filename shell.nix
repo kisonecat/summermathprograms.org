a07cc86887de86c7407d9b8245df30aef8e09e2b
Jim Fowler <fowler@math.osu.edu>
2025-05-04 17:41:03 -0400

    chore: Update Nix shell configuration for Haskell packages and tools

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@
 pkgs.haskellPackages.shellFor {
   # Ensure the project, including Hakyll, is available in your Nix environment
   packages = p: [ p.hakyll ];
-  tools = [ pkgs.stack pkgs.unzip ];
-  buildInputs = [ pkgs.zlib pkgs.pkg-config pkgs.gcc pkgs.haskellPackages.ghc8107 ];
+  tools = [ pkgs.unzip ];
+  buildInputs = [ pkgs.haskellPackages.stack pkgs.zlib pkgs.pkg-config pkgs.gcc pkgs.haskellPackages.ghc ];
   shellHook = ''
     export PKG_CONFIG_PATH="${pkgs.zlib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
     export C_INCLUDE_PATH="${pkgs.zlib.dev}/include:$C_INCLUDE_PATH"
